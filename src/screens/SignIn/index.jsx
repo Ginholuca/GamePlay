e525523ebbc7217d53aff65f1246e0acb8917852
Giovanni De Luca <ginholucacorrea@icloud.com>
2021-07-05 02:45:02 -0300

    test

---
+++ src/screens/SignIn/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import {View, Text} from "react-native";
+import {styles} from "./styles"
+
+export function App() {
+    <View style={styles.container}>
+        <Text>Ola mundo</Text>
+    </View>
+}
